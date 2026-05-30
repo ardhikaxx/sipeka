91b2b9c908a72afe8ad7ecf8abe0037fe1ea7341
ardhikaxx <ardhikayanuar58@gmail.com>
2026-05-30 12:34:16 +0700

    feat: penambahan fitur app/Models/Notifikasi.php

---
+++ app/Models/Notifikasi.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Notifikasi extends Model
+{
+    protected $fillable = [
+        'user_id',
+        'judul',
+        'pesan',
+        'is_read',
+    ];
+
+    protected $casts = [
+        'is_read' => 'boolean',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
